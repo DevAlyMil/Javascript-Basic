0f790e71d1a2859215993bc72516b781805d47e8
Alysson Miller <dev.alymiller@gmail.com>
2023-08-20 19:06:34 +0100

    two newexercices from day 4 of js

---
+++ dia 4/script.js
@@ -0,0 +1,64 @@
+
+// First chanllange day 4
+
+// 1.1 - Crie um sistema que pergunte ao seu usuário se ele está com fome, 
+// se tem dinheiro e se o restaurante está aberto.
+// 1.2 - Se estiver sem fome ou sem dinheiro exiba no console: 
+// "Hoje a janta será em casa"
+// 1.3 - Se estiver com fome e tiver dinheiro mas o restaurante estiver 
+// fechado exiba no console: "Peça um delivery!"
+// 1.4 - Se estiver com fome, tiver dinheiro e o restaurante estiver 
+// aberto exiba no console: "Hoje o jantar será no seu restaurante preferido!"
+
+// let hungry = prompt("are you hungry?");
+// let money = prompt("Do you have money?");
+// let restaurant = prompt("the restaurant is opended?");
+
+// if (hungry === "no" || money === "no"){
+//     console.log("Eat at home")
+// }else if(hungry === "yes" && money === "yes" && restaurant === "no"){
+//     console.log("Order a delivery")
+// }else{
+//     console.log("lets go to the nearest restaurant")
+// }
+ // Second challenge day 4
+
+// 1.1 - Crie um sistema que peça para o seu usuário as seguintes informações:
+//  nome, idade, se possuí carta de motorista e se tem algum carro.
+
+// 1.2 - Se o usuário não for maior de idade ou não tiver carta exiba no 
+// console: "Giovanni, você não pode dirigir".
+
+// 1.3 - Se o usuário for maior de idade, tiver carta mas não tiver carro 
+// exiba no console: "Giovanni, você pode dirigir mas não tem um carro".
+
+// 1.4 - Se o usuário for maior de idade, tiver carta e carro exiba no 
+// console: "Giovanni, você será o motorista!"
+
+// let userName = prompt("Insert your name")
+// let age = prompt("How old are you?")
+// let drive = prompt("Do you have drive license?")
+// let car = prompt("Do you have car?")
+
+
+// if (age < 18 || drive === "no"){
+//     console.log(userName + " You can not drive")
+// }else if (age >= 18 && drive === "yes" && car === "no"){
+//     console.log("Sorry " + userName + ", you can drive but you don`t have car")
+// }if(age >= 18 && drive === "yes" && car === "yes"){
+//     console.log(userName + ", you are driver")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
